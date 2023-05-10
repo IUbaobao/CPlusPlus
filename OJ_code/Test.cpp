3ff557ba7be28b2f800e913f12c2981790ebe140
iuvip <2868798970@qq.com>
2023-05-10 22:26:58 +0800

    oj-递增子序列

--- OJ_code/Test.cpp
+++ OJ_code/Test.cpp
@@ -64,32 +64,69 @@
 //示例 1：
 //输入：nums = [1, 2, 2]
 //输出： [[], [1], [1, 2], [1, 2, 2], [2], [2, 2]]
+//class Solution {
+//public:
+//    vector<vector<int>> reslut;
+//    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//        vector<int> path;
+//        vector<bool> used(nums.size(), false);//用来判断是否使用过这个数
+//        sort(nums.begin(), nums.end());
+//        backtracking(nums, path, 0, used);
+//        return reslut;
+//    }
+//
+//    void backtracking(vector<int>& nums, vector<int>& path, int starti, vector<bool>& used)
+//    {
+//        reslut.push_back(path);
+//        if (starti >= nums.size())
+//            return;
+//
+//        for (int i = starti; i < nums.size(); ++i)
+//        {
+//            if (i != 0 && nums[i - 1] == nums[i] && used[i - 1] == false)
+//                continue;
+//            path.push_back(nums[i]);
+//            used[i] = true;
+//            backtracking(nums, path, i + 1, used);
+//            used[i] = false;
+//            path.pop_back();
+//        }
+//    }
+////};
+
+
+
+//491. 递增子序列
+//给你一个整数数组 nums ，找出并返回所有该数组中不同的递增子序列，递增子序列中 至少有两个元素 。你可以按 任意顺序 返回答案。
+//数组中可能含有重复元素，如出现两个整数相等，也可以视作递增序列的一种特殊情况。
+//示例 1：
+//输入：nums = [4, 6, 7, 7]
+//输出： [[4, 6], [4, 6, 7], [4, 6, 7, 7], [4, 7], [4, 7, 7], [6, 7], [6, 7, 7], [7, 7]]
+
 class Solution {
 public:
     vector<vector<int>> reslut;
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int> path;
-        vector<bool> used(nums.size(), false);//用来判断是否使用过这个数
-        sort(nums.begin(), nums.end());
-        backtracking(nums, path, 0, used);
+    vector<int> path;
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        backtracking(nums, 0);
         return reslut;
     }
 
-    void backtracking(vector<int>& nums, vector<int>& path, int starti, vector<bool>& used)
+    void backtracking(vector<int>& nums, int stari)
     {
-        reslut.push_back(path);
-        if (starti >= nums.size())
-            return;
-
-        for (int i = starti; i < nums.size(); ++i)
+        if (path.size() > 1)
         {
-            if (i != 0 && nums[i - 1] == nums[i] && used[i - 1] == false)
+            reslut.push_back(path);
+        }
+        unordered_set<int> s;//本层去重
+        for (int i = stari; i < nums.size(); ++i)
+        {
+            if ((path.size() > 0 && nums[i] < path.back()) || s.find(nums[i]) != s.end())
                 continue;
+            s.insert(nums[i]);
             path.push_back(nums[i]);
-            used[i] = true;
-            backtracking(nums, path, i + 1, used);
-            used[i] = false;
+            backtracking(nums, i + 1);
             path.pop_back();
         }
     }
-//};
+};
